24032f8002d1521adec38eadef520ad8298c9628
Helen Ma <hhma@alumni.cpp.edu>
2026-07-07 22:39:10 -0400

    test commit comment July 7

--- src/test/java/guru/springframework/spring6webapp/Spring6WebappApplicationTests.java
+++ src/test/java/guru/springframework/spring6webapp/Spring6WebappApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Spring6WebappApplicationTests {
-    /**test July 6*/
+    /**test July 7*/
     @Test
     void contextLoads() {
     }
